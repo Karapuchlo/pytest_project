961d62c8f1c9507dcf05ae32f7565708044c6e40
aleksey <mc_gra_dy@mail.ru>
2023-02-06 14:21:49 +0300

    тесты теперь рабочие

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,7 +3,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get(["test",1,2], 0, "test") == "test"
 
 
 def test_slice():
